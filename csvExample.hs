98866927b91187a42974d0eee1c0d03f0611b4b3
kesslerjohn <john-kessler@uiowa.edu>
2024-04-13 11:26:03 -0500

    add attribution for example

--- csvExample.hs
+++ csvExample.hs
@@ -1,5 +1,7 @@
 import Text.ParserCombinators.Parsec
 
+-- Implemented following: https://book.realworldhaskell.org/read/using-parsec.html
+
 -- a CSV file contains 0 or more lines
 csvFile :: GenParser Char st [[String]]
 csvFile =
@@ -43,4 +45,5 @@ eol = char '\n'
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
 
-
+-- >>> parseCSV "c1,c2,c3\nr1c1,r1c2,r1c3\nr2c1,r2c2,r2c3\n"
+-- Right [["c1","c2","c3"],["r1c1","r1c2","r1c3"],["r2c1","r2c2","r2c3"]]
