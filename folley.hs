d325d13fddf36433aa558e891a0e1ab406694549
kesslerjohn <john-kessler@uiowa.edu>
2024-04-15 10:59:40 -0500

    add example output format

--- folley.hs
+++ folley.hs
@@ -10,4 +10,11 @@ p cnf 4 3 //format {# variables} {# clauses}
 3
 
 represents (x1 | x3 | -x4) & (x4) & (x2 | x3)
+
+
+-- example of output format --
+c Comment 1
+c Comment 2
+s SATISFIABLE // 
+v 1 2 3 4 // or {v -1 2 3 4}, {v -1 2 -3 4}, etc.
 --}
